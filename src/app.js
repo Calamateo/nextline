ee1fd01b2c87627540c1d34682b758c193d6bed5
Brian Calamateo <brian.calamateo@consulting.construction.corp>
2023-02-15 18:10:19 -0600

    Se añadio swagger para la documentación de las APIs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const createApp = () => {
   const app = express();
+
   app.use(express.json());
+  app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
   app.get('/', (req, res) => {
     res.send('Challenge nextline \nDaniel Calamateo');
   });
+
   return app;
 };
 
